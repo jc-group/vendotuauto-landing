2386f74e14d3ab29cadfe804ad0e951bfab54532
vcastr <victor_josafat.castro@daimlertruck.com>
2025-08-04 08:08:52 -0600

    feat: configure Vercel adapter with caching and image service settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    isr: {
+      expiration: 60 * 60 * 24, // cache por 1 día
+    },
+    imageService: true,
+    devImageService: 'sharp',
+    imagesConfig: {
+      sizes: [320, 640, 1280],
+    },
+    webAnalytics: { enabled: true },
+    // Puedes agregar más opciones según tus necesidades
+  }),
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
